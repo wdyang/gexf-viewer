d00cfef4a6f0e018adfa32cec7bb5efbad8bbd1b
Weidong Yang <wdyangmail@yahoo.com>
2012-10-23 14:58:26 -0700

    some change in initgraph.js

--- initgraph.js
+++ initgraph.js
@@ -160,6 +160,7 @@ function init(gexffile) {
 		  // var btn='<button class="btn-custom" id="'+ st[0]+ '">'+st[1]+'</button>';
 		  // info_text.append('<li>'+btn+'</li>');
 	  });
+	  AppObj.info_array = info_array;
 	  $('#info-panel').html(info_text).css({"opacity":0.6});
 	
 	  //When mouse over a name, the corresponding node will highlight.
@@ -195,11 +196,101 @@ function init(gexffile) {
 			
 			console.log(id);
 			sigInst.iterNodes(function(n){ n.color=n.attr.tcolor; });
-			drawDownNodes([id]);
+			addDownNodes([id]);
 		});
 
 		
     };
+
+	var addDownNodes=function(nodes){	  
+      var neighbors = {};
+	  var info_array=AppObj.info_array;	//inistialize with existing info
+      sigInst.iterEdges(function(e){
+        if(nodes.indexOf(e.source)<0 && nodes.indexOf(e.target)<0){   //edge doesn't include highlighted node
+		//do nothing in this case
+        }else{
+			e.color = e.source.color;
+			e.hidden=false;
+			neighbors[e.source] = 1;
+			neighbors[e.target] = 1;
+	  	}
+      }).iterNodes(function(n){
+        if(!neighbors[n.id]){    //node not in the neighbot list we do nothing
+        }else{
+          n.color = n.attr['grey'] ? n.attr['true_color'] : n.color;
+          n.attr['grey'] = 0;
+		  info_array.push([n.id, n.label]);
+        }
+      }).draw(2,2,2);
+	  
+	  //cleer up duplicates
+	  
+	  var get_uniq_info=function(a){
+		  for(i=1; i<a.length;){
+			  if (a[i-1][1]==a[i][1]){
+				  a.splice(i,1);
+			  }else{
+				  i++;
+			  }
+		  }
+		  return a;
+	  };
+	  
+	  var info_text=$('<ul>');
+	  
+	  var selectnode=sigInst.getNodes(nodes[0]);
+	  info_text.append('<li>'+selectnode['displayX']+" : "+selectnode['displayY']);
+	  info_array.sort(function(a,b){
+		  if( a[1]>b[1]) return 1;
+		  if (a[1]<b[1]) return -1;
+		  return 0;
+	  });
+	  info_array=get_uniq_info(info_array).forEach(function(st){
+		  info_text.append('<li id="'+st[0]+'">'+st[1]+'</li>');
+		  // var btn='<button class="btn-custom" id="'+ st[0]+ '">'+st[1]+'</button>';
+		  // info_text.append('<li>'+btn+'</li>');
+	  });
+	  $('#info-panel').html(info_text).css({"opacity":0.6});
+	
+	  //When mouse over a name, the corresponding node will highlight.
+	  	$('li').hover(function(){
+			window.li=$(this);
+			id=$(this)[0].id;
+			
+			console.log(id);
+			$(this).css({'font-size':20, 'color':'#000', 'background-color':'rgba(255,255,255,150)'});
+			sigInst.iterNodes(function(n){
+				if (n.id == id){
+					// n.attr['tcolor']=n.color;
+					n.color='#FFF';
+					n.attr['tsize']=n.size;
+					n.size=n.size*3 < nodeMaxSize ? n.size*3 : nodeMaxSize;
+				};
+			}).draw(2,2,2);
+	  		},function(){
+				$(this).css({'font-size':14, 'color':'#fff', 'background-color':'rgba(0,0,0,0)'});
+				id=$(this)[0].id;
+				sigInst.iterNodes(function(n){
+					if (n.id == id){
+						n.color=n.attr['tcolor'];
+						n.size=n.attr['tsize'];
+					};
+				}).draw(2,2,2);
+		  	}
+		);
+
+	  	$('li').click(function(){
+			window.li=$(this);
+			id=$(this)[0].id;
+			
+			console.log(id);
+			sigInst.iterNodes(function(n){ n.color=n.attr.tcolor; });
+			addDownNodes([id]);
+		});
+
+		
+    };
+
 
 
 	//Click on anywhere on the graph other than nodes will remove the highlights
